ef534f1dcc00d4657eefd0567c39e4febd5e6d8d
Sachin Rammoorthy <sachin.rammoorthy@gmail.com>
2022-05-26 11:43:59 -0400

    Integrated model

--- back_end/__main__.py
+++ back_end/__main__.py
@@ -6,6 +6,19 @@ import requests
 import sys
 import base64
 from io import BytesIO
+from dalle_mini import DalleBart, DalleBartProcessor
+from functools import partial
+
+import jax
+import random
+import numpy as np
+import jax.numpy as jnp
+from PIL import Image
+from vqgan_jax.modeling_flax_vqgan import VQModel
+
+from flax.jax_utils import replicate
+from flax.training.common_utils import shard_prng_key
+
 
 ### SETUP: FLASK ###
 app = Flask('gramophone')
@@ -47,20 +60,70 @@ image_collection = []
 phrase_collection = []
 
 
+# model
+
+DALLE_MODEL = "dalle-mini/dalle-mini/wzoooa1c:latest"
+DALLE_COMMIT_ID = None
+model = DalleBart.from_pretrained(DALLE_MODEL, revision=DALLE_COMMIT_ID)
+model._params = replicate(model.params)
+processor = DalleBartProcessor.from_pretrained(DALLE_MODEL, revision=DALLE_COMMIT_ID)
+
+# VQGAN model
+VQGAN_REPO = "dalle-mini/vqgan_imagenet_f16_16384"
+VQGAN_COMMIT_ID = "e93a26e7707683d349bf5d5c41c5b0ef69b677a9"
+vqgan = VQModel.from_pretrained(VQGAN_REPO, revision=VQGAN_COMMIT_ID)
+vqgan._params = replicate(vqgan.params)
+
+
+gen_top_k = None
+gen_top_p = 0.9
+temperature = None
+cond_scale = 3.0
+
+# model inference
+@partial(jax.pmap, axis_name="batch", static_broadcasted_argnums=(3, 4, 5, 6))
+def p_generate(tokenized_prompt, key, params, top_k, top_p, temperature, condition_scale):
+    return model.generate(
+        **tokenized_prompt,
+        prng_key=key,
+        params=params,
+        top_k=top_k,
+        top_p=top_p,
+        temperature=temperature,
+        condition_scale=condition_scale,
+    )
+
+
+# decode images
+@partial(jax.pmap, axis_name="batch")
+def p_decode(indices, params):
+    return vqgan.decode_code(indices, params=params)
+
+  
+def tokenize_prompt(prompt: str):
+  tokenized_prompt = processor([prompt])
+  return replicate(tokenized_prompt)
+
+
+
 ### APIs ###
-@app.route('/test', methods=['POST'])
+@app.route('/test', methods=['GET'])
 def test():
-    pass
+    return "Hello world"
     # how to return text and image? do we just save an image and return a path?
 
 @app.route('/speech2img', methods=['POST'])
 def speech2img():
-    response = deepgram_consoleASR(
-        request.get_data(), headers=request.headers,
-        request_kwargs=request.args
-    )
-    transcript = response.json()[
-        'results']['channels'][0]['alternatives'][0]['transcript']
+    # response = deepgram_consoleASR(
+    #     request.get_data(), headers=request.headers,
+    #     request_kwargs=request.args
+    # )
+    # transcript = response.json()[
+    #     'results']['channels'][0]['alternatives'][0]['transcript']
+
+    transcript = request.json["text"]
+    # print(transcript)
+    
     # pass transcript to `app.config['TEXT_TO_IMG_MODEL']`
     # how to return text and image? do we just save an image and return a path?
 
@@ -71,11 +134,32 @@ def speech2img():
     phrases_and_images = [{"phrase": phrase, "img": img} for phrase, img in zip(phrase_collection, image_collection)]
 
     return jsonify(phrases_and_images)
+    # return transcript
 
 
 def generate_image(prompt: str):
     # generate image from the text via DALL-E
-    pass
+    tokenized_prompt = tokenize_prompt(prompt)
+    
+    # create a random key
+    seed = random.randint(0, 2**32 - 1)
+    key = jax.random.PRNGKey(seed)
+
+    key, subkey = jax.random.split(key)
+    
+    encoded_image = p_generate(tokenized_prompt, shard_prng_key(subkey),
+        model.params, gen_top_k, gen_top_p, temperature, cond_scale,
+    )
+    
+    # remove BOS
+    encoded_image = encoded_image.sequences[..., 1:]
+
+    # decode images
+    decoded_image = p_decode(encoded_image, vqgan.params)
+    decoded_image = decoded_image.clip(0.0, 1.0).reshape((-1, 256, 256, 3))
+    image = Image.fromarray(np.asarray(img * 255, dtype=np.uint8))
+            
+    return image
 
 
 def imgToBase64(img):
